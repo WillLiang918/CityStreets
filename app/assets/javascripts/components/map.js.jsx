a0b11c3c7110cdc8f1da25027bdb7ae3c69c8de8
William <WilliamLiang918@gmail.com>
2015-11-19 14:46:40 -0500

    add marker to map.

--- app/assets/javascripts/components/map.js.jsx
+++ app/assets/javascripts/components/map.js.jsx
@@ -10,15 +10,36 @@
         center: { lat: 40.730610, lng: -73.935242 },
         zoom: 13
       };
-      this.markers = [];
+      this.markers = null;
       this.map = new google.maps.Map(map, mapOptions);
       this.registerListeners();
+      MarkerStore.addChangeListener(this._onChange);
+    },
+
+    _onChange: function () {
+      this.removeMarker(this.marker);
+      this.createMarker(MarkerStore.getMarker());
     },
 
     registerListeners: function () {
       var that = this;
       google.maps.event.addListener(this.map, 'idle', function() {
       });
+    },
+
+    createMarker: function (coords) {
+      var that = this;
+      var pos = new google.maps.LatLng(coords.lat, coords.lng);
+      this.marker = new google.maps.Marker({
+        position: pos,
+        map: that.map,
+      });
+    },
+
+    removeMarker: function (marker) {
+      if (marker) {
+        marker.setMap(null);
+      }
     },
 
     render: function () {
