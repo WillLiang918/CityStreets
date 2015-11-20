899443f5598281f4abf138b683d236bc602847df
William <WilliamLiang918@gmail.com>
2015-11-20 13:16:05 -0500

    edited gitignore.

--- app/assets/javascripts/components/property_show.js.jsx
+++ app/assets/javascripts/components/property_show.js.jsx
@@ -22,7 +22,6 @@
       var propertyId = this.props.params.propertyId;
       var property = this._findPropertyById(propertyId);
       this.setState({ property: property });
-      console.log(this.state);
     },
 
     componentDidMount: function () {
@@ -35,19 +34,19 @@
     },
 
     render: function () {
-      var propertyFound;
-      var that = this;
-
-      if (that.state.property.id == this.state.property.id ) {
-        debugger
-        propertyFound = <Detail property={that.state.property} />;
-      } else {
-        propertyFound = <div></div>;
-      }
+      // var propertyFound;
+      // var that = this;
+      //
+      // if (that.state.property.id == this.state.property.id ) {
+      //   propertyFound = <Detail property={that.state.property} />;
+      // } else {
+      //   propertyFound = <div></div>;
+      // }
 
       return (
-        <div>
-          {propertyFound}
+        <div className="property-page">
+          <SlideShow property={this.state.property} />
+          <Detail property={this.state.property} />
         </div>
       );
     }
---
+++ app/assets/javascripts/components/slide_show.js.jsx
@@ -0,0 +1,11 @@
+(function (root) {
+
+  var SlideShow = root.SlideShow = React.createClass ({
+    render: function () {
+      return (
+        <div className="slide-show">
+        </div>
+      );
+    }
+  });
+})(this);
