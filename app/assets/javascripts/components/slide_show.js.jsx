2ceef00a98ee124ac53499eb920857236c464927
William <WilliamLiang918@gmail.com>
2015-11-20 22:39:52 -0500

    Add image gallery to show page.

--- app/assets/javascripts/components/slide_show.js.jsx
+++ app/assets/javascripts/components/slide_show.js.jsx
@@ -2,31 +2,31 @@
 
   var SlideShow = root.SlideShow = React.createClass ({
     getInitialState: function () {
-      return { photos: [] };
+      return { currentphoto: "",
+               photos: [] };
     },
 
     _getPhotos: function () {
       var id = this.props.property.id;
       var photos = PhotoStore.find(id);
       if (photos.length > 0 ) {
-        this.setState({ photos: photos});
-        debugger
+        this.setState({
+          photos: photos,
+          currentPhoto: photos[0].image_url
+        });
       }
     },
 
     componentDidMount: function () {
       PhotoStore.addChangeListener(this._getPhotos);
       ApiUtil.fetchPhotos(PropertyStore.getIds);
-      // this._getPhotos();
     },
 
     componentWillUnmount: function () {
       PhotoStore.removeChangeListener(this._getPhotos);
     },
 
-
-
-    // componentDidMount: function () {
+    // componentDidMount: function ()
     //   ApiUtil.fetchPhotos();
     //   propertyId = this.props.property.id;
     //   var photos = PhotoStore.find(propertyId);
@@ -35,13 +35,18 @@
 
     render: function () {
       var that = this;
+
       return (
         <div className="slide-show">
-        {
-          that.state.photos.map( function (photo) {
-            return <img src={photo.image_url}/>;
-          })
-        }
+          <img className="selected-photo"
+               src={this.state.currentPhoto} />
+          <ul>
+            {
+              that.state.photos.map( function (photo) {
+                return <li><img className="thumb" src={photo.image_url}/></li>;
+              })
+            }
+          </ul>
         </div>
       );
     }
