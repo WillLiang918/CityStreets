b48073fc364e459f0a7172238854abc8b7108f69
William <WilliamLiang918@gmail.com>
2015-12-08 08:27:19 -0500

    Remove video audio and style splash page.

--- app/assets/javascripts/components/splash_page.js.jsx
+++ app/assets/javascripts/components/splash_page.js.jsx
@@ -25,27 +25,35 @@
     componentWillUnmount: function () {
       CurrentUserStore.removeChangeListener(this.onChange);
     },
-
+          //  <iframe
+          //   src="//player.vimeo.com/video/96396015?title=0&amp;byline=0&amp;portrait=0&amp;color=3a6774&amp;autoplay=1&amp;loop=1&amp;muted=1"
+          //   width="960" height="540" frameBorder="0" webkitallowfullscreen
+          //   mozallowfullscreen allowFullScreen>
+          //  </iframe>
     render: function () {
       return (
         <div>
-         <div className="video">
-           <iframe
-            src="//player.vimeo.com/video/96396015?title=0&amp;byline=0&amp;portrait=0&amp;color=3a6774&amp;autoplay=1&amp;loop=1&amp;muted=1"
-            width="960" height="540" frameBorder="0" webkitallowfullscreen
-            mozallowfullscreen allowFullScreen>
-           </iframe>
-          <div className="overlay"></div>
+         <div className="video-container">
+           <video
+            className="home-page-splash-video"
+            src="http://s3.amazonaws.com/city-streets-development/videos/background.m4v"
+            autoPlay
+            type="video/mp4">
+          </video>
         </div>
-        <div className="splash-content">
-          <MainHeader
-            currentUser={ this.state.currentUser }
-            auth={ this.state.auth }
-            toggleAuth={ this.toggleAuth }
-            history={ this.props.history }/>
-          <h3 className="splash-header">New York City Real Estate</h3>
-          <SearchForm
-            history={ this.history }/>
+        <div className="splash-content-container">
+          <div className="splash-content">
+            <MainHeader
+              currentUser={ this.state.currentUser }
+              auth={ this.state.auth }
+              toggleAuth={ this.toggleAuth }
+              history={ this.props.history }/>
+            <div className="splash-page-search">
+              <h3 className="splash-header">New York City Real Estate</h3>
+              <SearchForm
+                history={ this.history }/>
+              </div>
+            </div>
           </div>
         </div>
       );
